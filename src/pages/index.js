5c7908aab41a5e445fd7f2479359871303b829a9
Gastón Mira <gastonmira@gmail.com>
2023-03-18 19:32:57 -0300

    Save index.js.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
+import {
+    Helmet
+} from 'react-helmet';
 import App from '../components/App';
-import { headData } from '../mock/data';
+import {
+    headData
+} from '../mock/data';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../style/main.scss';
 
 export default () => {
-  const { title, lang, description } = headData;
+    const {
+        title,
+        lang,
+        description
+    } = headData;
 
-  return (
-    <>
-      <Helmet>
+    return (
+        <>
+        <Helmet>
         <meta charSet="utf-8" />
-        <title>{title || 'Gaston Mira | Android dev | Software Engineer'}</title>
+        <title>{title || 'Gaston Mira | Software Engineer | Manager'}</title>
         <html lang={lang || 'en'} />
-        <meta name="title" content='Gaston Mira | Android dev | Software Engineer' />
-        <meta name="titleTemplate" content='Gaston Mira - Android developer from Argentina' />
-        <meta name="description" content={description || 'Android developer from Mar del Plata, Argentina.'} />
+        <meta name="title" content='Gaston Mira | Software Engineer | Manager' />
+        <meta name="titleTemplate" content='Gaston Mira - Software Engineer from Argentina' />
+        <meta name="description" content={description || 'Software Engineer from Mar del Plata, Argentina.'} />
         <meta name="url" content='https://gastonmira.dev/' />
         <meta name="image" content='./images/gastonPerfilWeb.jpg' />
       </Helmet>
-      <App />
-    </>
-  );
+        <App /> < />
+    );
 };
